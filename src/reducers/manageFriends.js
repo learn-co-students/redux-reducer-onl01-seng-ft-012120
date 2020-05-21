b90b16b6ccd538da1ead6494cb8ba0b19e6e9f7d
Patrick Grady <patickrgrady81@gmail.com>
2020-05-21 09:15:04 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,8 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) { 
+    case "ADD_FRIEND":
+      return { friend: Object.assign({}, state.friends, action.type.friend) }
+    default:
+      return state;
+  }
 }
